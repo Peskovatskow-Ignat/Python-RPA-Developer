77e344ec2cddc3127fb5cbb07a33cd47ab7e8c8f
Ignat <pesk2305@yandex.ru>
2024-04-01 07:04:14 +0300

    add celery

---
+++ webapp/integrations/celery_app/celery.py
@@ -0,0 +1,9 @@
+from celery import Celery
+
+app_celery = Celery(
+    "worker",
+    broker="redis://redis:6379",
+    include=["webapp.integrations.celery_app.tasks"],
+    debug=True,
+    backend="redis://redis:6379"
+)
---
+++ webapp/integrations/celery_app/tasks.py
@@ -0,0 +1,15 @@
+import asyncio
+
+from webapp.integrations.celery_app.celery import app_celery
+
+
+@app_celery.task()
+def start_robot(data: dict[str, any]) -> None:
+    start = data.get('start', 0)
+    task_id = data.get('task_id')
+    while True:
+        print(f'task_id: {task_id} | iter_number: {start}')
+        start += 1
+        from time import sleep
+        sleep(1)
+
